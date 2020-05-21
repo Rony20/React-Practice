f2ed332b45ff96434b61c6b221698e774e6f0f54
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-05-21 18:19:26 +0530

    Passing data to event handler using two methods. Bind and arrow function. In which bind method is more useful.

--- src/App.js
+++ src/App.js
@@ -8,16 +8,15 @@ class App extends Component {
   state = {
     persons: [
       {name: 'Rohan', age: 21},
-      {name: 'Sneh', age: 18},
-      {name: 'Kevin', age: 17}
+      {name: 'Kevin', age: 18},
+      {name: 'Sneh', age: 17}
     ]
   }
 
-  switchNameHandler = () => {
-    console.log("Was Clicked")
+  switchNameHandler = (newName) => {
     this.setState({
       persons: [
-        {name: 'Rohan Borakhatariya', age: 21},
+        {name: newName, age: 21},
         {name: 'Kevin', age: 18},
         {name: 'Sneh', age: 18}
       ]
@@ -29,10 +28,21 @@ class App extends Component {
       <div className="App">
         <h1>Hello World !</h1>
         <p>This is working !</p>
-        <button onClick={this.switchNameHandler}> Switch Name</button>
-        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }/>
-        <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }>I want to join INA.</Person>
-        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age }/>
+        <button onClick={() => this.switchNameHandler('Rohan Borakhatariya')}> Switch Name</button>
+        <Person 
+          name = { this.state.persons[0].name } 
+          age = { this.state.persons[0].age }
+        />
+        <Person 
+          click = { this.switchNameHandler.bind(this, 'Rony') }
+          name = { this.state.persons[1].name } 
+          age = { this.state.persons[1].age }>
+            I want to join INA.
+        </Person>
+        <Person 
+          name = { this.state.persons[2].name }
+          age = { this.state.persons[2].age }
+        />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('header', {className: 'App-header'}, 'Hello World !!'))
