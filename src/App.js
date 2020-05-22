ae3a8f4cb2234969dd5ad7ca4d7c31368113a465
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-05-22 18:24:28 +0530

    Flexible lists for changing name on input change. Key error is removed by providing key as unique id.

--- src/App.js
+++ src/App.js
@@ -7,25 +7,32 @@ class App extends Component {
 
   state = {
     persons: [
-      {name: 'Rohan', age: 21},
-      {name: 'Kevin', age: 18},
-      {name: 'Sneh', age: 17}
+      {id:"n1", name: 'Rohan', age: 21},
+      {id:"n2", name: 'Kevin', age: 18},
+      {id:"n3", name: 'Sneh', age: 17}
     ],
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {name: 'Rohan', age: 21},
-        {name: event.target.value, age: 18},
-        {name: 'Sneh', age: 18}
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(per => per.id === id)
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value
+
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
+
+    this.setState({persons: persons})
   }
 
   deletePersonhandler = (personIndex) => {
-    const persons = this.state.persons
+    // const persons = this.state.persons.slice() copies the array instead of passing reference only.
+    const persons = [...this.state.persons]
     persons.splice(personIndex, 1)
     this.setState({persons: persons})
   }
@@ -38,7 +45,8 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'blue',
+      color: 'white',
       font: "inherit",
       border: "1px solid blue",
       padding: '8px',
@@ -53,8 +61,10 @@ class App extends Component {
           { this.state.persons.map( (person, index) => {
             return <Person 
               click = { this.deletePersonhandler.bind(this, index) }
+              changed = {(event) => this.nameChangedHandler(event, person.id)}
               name = {person.name} 
-              age = {person.age}/>
+              age = {person.age}
+              key = { person.id }/>
           }) }
         </div>
       )
