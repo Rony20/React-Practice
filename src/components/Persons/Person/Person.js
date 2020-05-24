31a957dd1834ea7341d45f3a044b50392b40988c
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-05-24 17:59:45 +0530

    Using new higher order wrapping element to wrap person info. Because in the we don't need the div to be there then we can use this approch.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
 
+import Aux from "../../HOC/Auxiliary"
 import classes from "./Person.module.css";
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...')
     return (
-      <div className={classes.Person}>
+      <Aux className={classes.Person}>
         <p onClick={this.props.click}>
           I'm {this.props.name}. I'm {this.props.age} years old !
         </p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
+      </Aux>
     );
   }
 };
