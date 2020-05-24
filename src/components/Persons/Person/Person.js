07465e079e21ad6e5fab54928c6acf173169718b
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-05-24 22:44:56 +0530

    Prop type check is added.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types"
 
 import classes from "./Person.module.css";
 
@@ -6,15 +7,22 @@ class Person extends Component {
   render() {
     console.log('[Person.js] rendering...')
     return (
-      <Fragment>
+      <div className={classes.Person}>
         <p onClick={this.props.click}>
           I'm {this.props.name}. I'm {this.props.age} years old !
         </p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </Fragment>
+      </div>
     );
   }
 };
+
+Person.propTypes = {
+  click: PropTypes.func,
+  changed: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number
+}
 
 export default Person;
