38cecb30acc346d67cd4c104b38b09949b7e78fd
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-05-24 18:07:00 +0530

    Using React Fragment component instead of custom higher order component. Although css will not work here because we cannot give classname to it.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
-import Aux from "../../HOC/Auxiliary"
 import classes from "./Person.module.css";
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...')
     return (
-      <Aux className={classes.Person}>
+      <Fragment>
         <p onClick={this.props.click}>
           I'm {this.props.name}. I'm {this.props.age} years old !
         </p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </Aux>
+      </Fragment>
     );
   }
 };
